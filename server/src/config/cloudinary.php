10eae3f7fd50168ffa85095bd472c5b85b7a7b0e
Kawasaki <iduchungho@gmail.com>
2023-01-24 00:07:52 +0700

    fix cloudinary

---
+++ server/src/config/cloudinary.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Use the Configuration class 
+use Cloudinary\Configuration\Configuration;
+
+// Configure an instance of your Cloudinary cloud
+// Configuration::instance('cloudinary://935594315273852:QwzZr3s2shCyDZQWKSCYo3wZnBw@dqsiqqz7q?secure=true');
+    class Cloud {
+        private $mykey = '935594315273852';
+        private $mysecret = 'QwzZr3s2shCyDZQWKSCYo3wZnBw';
+        private $myCloudName = 'dqsiqqz7q';
+        private $mySecure = 'true';
+
+        public function __construct() {
+            Configuration::instance("cloudinary://$this->mykey:$this->mysecret@$this->myCloudName?secure=$this->mySecure");
+            echo json_encode(
+                array("message" => "cloudinary connected successfully")
+            );
+        }
+
+    }
