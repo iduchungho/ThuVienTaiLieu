a637fc7cbb0cba0be2581eb6245be8fa0a3d19b9
namnguy1 <namepchai1221@gmail.com>
2023-03-28 16:31:17 +0700

    28/3

--- server/src/api/menu/deleteMenubyID.php
+++ server/src/api/menu/deleteMenubyID.php
@@ -3,12 +3,13 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: GET');
 
+
 include_once '../../config/database.php';
 include_once '../../models/menu.php';
 
 $database = new Database();
 $db = $database->connect();
-$id = $_GET["menu_id"];
+$id = $_GET['menu_id'];
 $api = new Menu($db);
 $api->delete_menu($id);
 
