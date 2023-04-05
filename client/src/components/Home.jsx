fa555a48ac89aa5d3efb14c9270d12840489ea4b
Kayden <trungtin02tkt1@gmail.com>
2023-04-05 16:30:12 +0700

    Update Routes

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainContainer from './MainContainer.jsx';
+import Header from './Header';
+
+const Home = () => {
+  return (
+    <div className="w-screen h-auto flex flex-col bg-primary">
+      <Header />
+      <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+        <Routes>
+          <Route path="/" element={<MainContainer />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
