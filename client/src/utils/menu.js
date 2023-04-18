789e086cb410cd158ec12c06a72abb6a4fd2f785
Kawasaki <iduchungho@gmail.com>
2023-04-18 22:07:19 +0700

    update axios

--- client/src/utils/menu.js
+++ client/src/utils/menu.js
@@ -1,1 +1,62 @@
+import axios from "axios";
 import { url } from "./customer";
+
+export const CreateMenu = async (input, id) => {
+    try {
+        const {data} = await axios.post(`${url}/api/menu/createMenu.php?id=${id}`, input)
+        return data;
+    }
+    catch(err){
+        return false
+    }
+}
+
+export const DeleteAll = async (id) => {
+    try {
+        const {data} = await axios.delete(`${url}/api/menu/deleteAll.php?id=${id}`)
+        return data
+    }
+    catch(err){
+        return false
+    }
+}
+
+export const DeleteMenuById = async (id, menu_id) => {
+    try {
+        const {data} = await axios.delete(`${url}/api/menu/deleteById.php?id=${id}&menu_id=${menu_id}`)
+        return data
+    }
+    catch(err){
+        return false
+    }
+}
+
+export const GetMenu = async () => {
+    try {
+        const {data} = await axios.get(`${url}/api/menu/getMenu.php`)
+        return data
+    }
+    catch(err){
+        return false
+    }
+}
+
+export const GetMenuById = async (id) => {
+    try{
+        const {data} = await axios.get(`${url}/api/menu/getMenuById.php?menu_id=${id}`)
+        return data
+    }
+    catch(err){
+        return false
+    }
+}
+
+export const UpdateMenu = async (input) => {
+    try {
+        const { data } = await axios.put(`${url}/api/menu/updateMenu.php`, input)
+        return data
+    }
+    catch (err) {
+        return false
+    }
+}
--- client/src/utils/order.js
+++ client/src/utils/order.js
@@ -1,3 +1,62 @@
 import axios from "axios";
 import { url } from "./customer";
 
+export const CreateOrder = async (id, input) => {
+    try {
+        const {data} = await axios.post(`${url}/api/order/create.php?id=${id}`, input)
+        return data
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+export const DeleteAllOrder = async (id) => {
+    try {
+        const { data } = await axios.delete(`${url}/api/order/deleteAll.php?id=${id}`)
+        return data
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+export const DeleteOrderById = async (id, order_id) => {
+    try {
+        const { data } = await axios.delete(`${url}/api/order/deleteByid.php?id=${id}&order_id=${order_id}`)
+        return data
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+export const GetAllOrder = async (id) => {
+    try {
+        const { data } = await axios.get(`${url}/api/order/getALL.php?id=${id}`)
+        return data
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+export const GetOrderByID = async (id, order_id) => {
+    try {
+        const { data } = await axios.get(`${url}/api/order/getALL.php?id=${id}&order_id=${order_id}`);
+        return data
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+export const UpdateOrder = async (id) => {
+    try {
+        const { data } = await axios.get(`${url}/api/order/update.php?id=${id}`)
+        return data
+    }
+    catch (err){
+        return false
+    }
+}
