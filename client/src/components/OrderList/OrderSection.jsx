f1c33f4e420876a6beee02cb182dce0a4c70ea29
Kayden <trungtin02tkt1@gmail.com>
2023-04-25 22:50:34 +0700

    fix

--- client/src/components/OrderList/OrderSection.jsx
+++ client/src/components/OrderList/OrderSection.jsx
@@ -1,50 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { get_order_by_ctmid } from './../../utils/order';
+import { GetMenuById } from './../../utils/menu';
 
 OrderSection.propTypes = {};
-const products = [
-  {
-    id: 1,
-    name: 'Nomad Tumbler',
-    description:
-      'This durable and portable insulated tumbler will keep your beverage at the perfect temperature during your next adventure.',
-    href: '#',
-    price: '35.00',
-    status: 'Preparing to ship',
-    step: 1,
-    date: 'March 24, 2021',
-    datetime: '2021-03-24',
-    address: ['Floyd Miles', '7363 Cynthia Pass', 'Toronto, ON N3Y 4H8'],
-    email: 'f•••@example.com',
-    phone: '1•••••••••40',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/confirmation-page-03-product-01.jpg',
-    imageAlt: 'Insulated bottle with white base and black snap lid.',
-  },
-  {
-    id: 2,
-    name: 'Minimalist Wristwatch',
-    description: 'This contemporary wristwatch has a clean, minimalist look and high quality components.',
-    href: '#',
-    price: '149.00',
-    status: 'Shipped',
-    step: 0,
-    date: 'March 23, 2021',
-    datetime: '2021-03-23',
-    address: ['Floyd Miles', '7363 Cynthia Pass', 'Toronto, ON N3Y 4H8'],
-    email: 'f•••@example.com',
-    phone: '1•••••••••40',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/confirmation-page-03-product-02.jpg',
-    imageAlt:
-      'Arm modeling wristwatch with black leather band, white watch face, thin watch hands, and fine time markings.',
-  },
-  // More products...
-];
-
 function OrderSection(props) {
-  const getOrder = async () => {
-    // const data = await
-  };
+  const [order, setOrder] = useState([]);
+  const [products, setProducts] = useState([]);
+  const loggedInUser = useSelector((state) => state.user.current);
+  useEffect(() => {
+    const getOrder = async () => {
+      const data = await get_order_by_ctmid(loggedInUser.customer_id);
+      setOrder(data.data);
+      for (let i = 0; i < data.data.length; i++) {
+        const product1 = await GetMenuById(data.data[i].menu_id);
+        setProducts([...products, product1]);
+      }
+    };
+    getOrder();
+  }, []);
 
+  console.log(products);
   return (
     <div className="bg-gray-50 mt-6">
       <div className="max-w-2xl mx-auto pt-16 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -69,47 +45,35 @@ function OrderSection(props) {
 
           <div className="space-y-8">
             {products.map((product) => (
-              <div
-                key={product.id}
-                className="bg-white border-t border-b border-gray-200 shadow-sm sm:border sm:rounded-lg"
-              >
+              <div className="bg-white border-t border-b border-gray-200 shadow-sm sm:border sm:rounded-lg">
                 <div className="py-6 px-4 sm:px-6 lg:grid lg:grid-cols-12 lg:gap-x-8 lg:p-8">
                   <div className="sm:flex lg:col-span-7">
                     <div className="flex-shrink-0 w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-none sm:w-40 sm:h-40">
                       <img
                         src={product.imageSrc}
-                        alt={product.imageAlt}
+                        alt="product"
                         className="w-full h-full object-center object-cover sm:w-full sm:h-full"
                       />
                     </div>
 
                     <div className="mt-6 sm:mt-0 sm:ml-6">
                       <h3 className="text-base font-medium text-gray-900">
-                        <a href={product.href}>{product.name}</a>
+                        <div>{product.menu_name}</div>
                       </h3>
                       <p className="mt-2 text-sm font-medium text-gray-900">${product.price}</p>
-                      <p className="mt-3 text-sm text-gray-500">{product.description}</p>
+                      <p className="mt-3 text-sm text-gray-500">
+                        This durable and portable insulated tumbler will keep your beverage at the perfect temperature
+                        during your next adventure.
+                      </p>
                     </div>
                   </div>
 
                   <div className="mt-6 lg:mt-0 lg:col-span-5">
                     <dl className="grid grid-cols-2 gap-x-6 text-sm">
                       <div>
-                        <dt className="font-medium text-gray-900">Delivery address</dt>
+                        <dt className="font-medium text-gray-900">Order Status</dt>
                         <dd className="mt-3 text-gray-500">
-                          <span className="block">{product.address[0]}</span>
-                          <span className="block">{product.address[1]}</span>
-                          <span className="block">{product.address[2]}</span>
-                        </dd>
-                      </div>
-                      <div>
-                        <dt className="font-medium text-gray-900">Shipping updates</dt>
-                        <dd className="mt-3 text-gray-500 space-y-3">
-                          <p>{product.email}</p>
-                          <p>{product.phone}</p>
-                          <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
-                            Edit
-                          </button>
+                          <span className="block">Confirmed</span>
                         </dd>
                       </div>
                     </dl>
